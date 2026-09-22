0edce3a74430e9e9ea18ad750f6f2ef8f7eb5987
luiza.gonzalez-v <luiza.gonzalez-v@INF-D310-019.labinfo.is.escuelaing.edu.co>
2026-09-22 09:07:28 -0500

    feat: Create scaffolding

---
+++ src/main/java/co/edu/eci/virtualizationlaboratorio/VirtualizationLaboratorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.eci.virtualizationlaboratorio;
+
+/**
+ *
+ * @author luiza.gonzalez-v
+ */
+public class VirtualizationLaboratorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
